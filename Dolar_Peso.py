f50d6cd778819a06367556bd159af231c6b76ac4
aixa-source <aixa8868@gmail.com>
2025-12-18 07:27:25 -0600

    convertidor mildred

---
+++ Dolar_Peso.py
@@ -0,0 +1,4 @@
+dolares = float(input("Dólares: "))
+tipo_cambio = 17.97  
+pesos = dolares * tipo_cambio
+print(pesos)
---
+++ Peso_Dolar.py
@@ -0,0 +1,4 @@
+pesos = float(input("Pesos: "))
+tipo_cambio = 17.97 
+dolares = pesos / tipo_cambio
+print(dolares)
